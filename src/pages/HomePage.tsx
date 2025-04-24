325639a5ab0791f8e04f23d3855e239c33a4bd88
JiwonRyu <jiwoni1210@naver.com>
2025-04-24 18:34:36 +0900

    Feat: 홈페이지 ui 구현

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,3 +1,30 @@
-const HomePage = () => <div>홈 페이지</div>;
+import Carousel from '@/components/home/Carousel';
+import PostList from '@/components/home/PostList';
+import { useDeviceStore } from '@/store/useDeviceStore';
+
+const HomePage = () => {
+  const responsivePaddingClass = useDeviceStore((state) => state.responsivePaddingClass);
+
+  return (
+    <main className="flex flex-col items-center gap-10 py-8 md:py-16 lg:py-24 bg-[#F5F5F5]">
+      <header className="text-center mb-4">
+        <h2 className="text-[#5E616E] text-md sm:text-lg font-bold mb-3">다글제작소</h2>
+        <h1 className="text-2xl sm:text-3xl font-bold leading-relaxed">
+          다글제작소의 과제전형에
+          <br />
+          오신 것을 환영합니다.
+        </h1>
+      </header>
+
+      <section className={`w-full max-w-[1400px] overflow-hidden ${responsivePaddingClass}`}>
+        <Carousel />
+      </section>
+
+      <section className={`w-full ${responsivePaddingClass}`}>
+        <PostList />
+      </section>
+    </main>
+  );
+};
 
 export default HomePage;
