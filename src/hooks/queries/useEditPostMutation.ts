a8920a0793dafad92581e93dd233f7d273b7b455
jiwonryu <jiwoni1210@naver.com>
2025-04-26 17:53:01 +0900

    Feat: 게시글 수정시 이동경로 추가

--- src/hooks/queries/useEditPostMutation.ts
+++ src/hooks/queries/useEditPostMutation.ts
@@ -1,6 +1,8 @@
 import { patchPost } from '@/api/post/patchPost';
 import { QUERY_KEYS } from '@/constants/api';
+import { PATH } from '@/constants/path';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useNavigate } from 'react-router-dom';
 
 interface EditPostParams {
   postId: string;
@@ -11,12 +13,13 @@ interface EditPostParams {
 // 게시글 수정
 export const useEditPostMutation = () => {
   const queryClient = useQueryClient();
-
+  const navigate = useNavigate();
   return useMutation({
     mutationFn: ({ postId, title, content }: EditPostParams) => patchPost(postId, title, content),
     onSuccess: (_, variables) => {
       queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.GET_POST_DETAIL, variables.postId] });
       queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.GET_POSTS] });
+      navigate(`${PATH.DETAIL_POST(variables.postId)}`);
     },
   });
 };
